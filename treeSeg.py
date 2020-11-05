5e92adf4971c6222800a6698526e1af03f0a48bd
Adam Conrad <conraada@oregonstate.edu>
2020-11-05 12:26:33 -0800

    More usable and added comments

--- treeSeg.py
+++ treeSeg.py
@@ -32,37 +32,59 @@ def extract_color(hsv, lower, upper):
     mask = cv2.inRange(hsv, lower, upper)
     imask = mask>0
     green = np.zeros_like(hsv, np.uint8)
-    # green = np.full_like(hsv, 255, np.uint8)
     green[imask] = hsv[imask]
     rgb = cv2.cvtColor(green, cv2.COLOR_HSV2RGB)
     rgb[~imask] = [255, 255, 255]
     return rgb
 
-
 def main():
 
+## Path to data
  path = "data/plantation1.tif"
 
- # Read data, optionally downscale for faster proccessing
- rgb = imread(path)
- # image0 = skimage.io.imread(fname=path, as_gray=True)
+## Percent of data to use
+ downscale_percent = 10
 
- # rgb0 = downscale(rgb, 10)
- rgb0 = downscale(rgb[:int(rgb.shape[0]/3), int(rgb.shape[1]/3):], 20)
- print(rgb0.shape)
+## Arbitrary scaling factor for Felzenszwalb segmentation, lower is more accurate and slower
+ segment_size_factor = 500
 
- # Convert to HSV color space for easier feature detection
- # hsv = cv2.cvtColor(rgb0, cv2.COLOR_RGB2HSV)
+## Read data and downscale for faster proccessing
+ rgb0 = imread(path)
+ rgb = downscale(rgb0, downscale_percent)
+ # rgb0 = downscale(rgb[:int(rgb.shape[0]/3), int(rgb.shape[1]/3):], 20)
+ print(rgb.shape)
 
- ## Scale lower scale is more accurate and much slower
- image_felzenszwalb = seg.felzenszwalb(rgb0, scale=100) ## returns region label of every pixel
+## Segment image
+ image_felzenszwalb = seg.felzenszwalb(rgb, scale=segment_size_factor) ## returns region label of every pixel
  # print(image_felzenszwalb)
  regions = np.unique(image_felzenszwalb, return_index=True, return_inverse=True, return_counts=True)
  # indecies = regions[1]
  # inv_inds = regions[2]
  # print(inv_inds.shape)
  print(f"Felzenszwalb shape: {image_felzenszwalb.shape}")
- felz = color.label2rgb(image_felzenszwalb, rgb0, kind='avg')
+
+## Use original image to color the segments based on label
+ felz = color.label2rgb(image_felzenszwalb, rgb, kind='avg')
+
+## Display results
+ f, axarr = plt.subplots(1,2)
+ axarr[0].imshow(rgb)
+ axarr[1].imshow(felz)
+
+ print("--- %s seconds ---" % (time.time() - start_time))
+ plt.show()
+
+main()
+
+### Other stuff I was playing with:
+
+ # Convert to HSV color space for easier feature detection
+ # hsv = cv2.cvtColor(rgb0, cv2.COLOR_RGB2HSV)
+
+## Select color range and return rgb
+ # rgb0 = extract_color(hsv, (0, 0, 0), (255, 255, 255))
+ # rgb1 = extract_color(hsv, (36, 25, 25), (70, 255, 255))
+ # rgb2 = extract_color(hsv, (30, 0, 0), (90, 255, 255))
 
  # edges = skimage.feature.canny(
  #     image=image,
@@ -94,24 +116,3 @@ def main():
  # filtered0 = gaussian_filter(rgb0, sigma=sig)
  # filtered1 = gaussian_filter(rgb0, sigma=5)
  # filtered2 = gaussian_filter(rgb0, sigma=10)
-
-
-
- f, axarr = plt.subplots(1,2)
- axarr[0].imshow(rgb0)
- axarr[1].imshow(felz)
-
- print("--- %s seconds ---" % (time.time() - start_time))
- plt.subplots_adjust(left=None, bottom=None, right=None, top=None, wspace=None, hspace=None)
-
- plt.show()
-
-
-
-main()
-
-
-## Select color range and return rgb
- # rgb0 = extract_color(hsv, (0, 0, 0), (255, 255, 255))
- # rgb1 = extract_color(hsv, (36, 25, 25), (70, 255, 255))
- # rgb2 = extract_color(hsv, (30, 0, 0), (90, 255, 255))
